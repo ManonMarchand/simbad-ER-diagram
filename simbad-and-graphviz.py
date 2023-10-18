38324536a76ba34d3938ed8051e7135c210f8f76
MARCHAND MANON <manonmarchand22@gmail.com>
2023-10-18 14:36:00 +0200

    add colors for edges

--- simbad-and-graphviz.py
+++ simbad-and-graphviz.py
@@ -2,8 +2,10 @@
 
 import pyvo
 import graphviz
+from itertools import cycle
 
 simbadtap = pyvo.dal.TAPService("https://simbad.unistra.fr/simbad/sim-tap")
+colors = cycle(["#9EADC8", "#C7FFDA", "#FEC1A7", "#4CE0B3", "#F0A8ED", "#FDD791", "#F4B6C2", "#FE9599", "#FFF196"])
 
 #------------
 # Retrieving data
@@ -32,7 +34,7 @@ measurement_tables = simbadtap.run_sync(query).to_table()
 simbad_er = graphviz.Graph("Simbad Relational Database", format='svg', engine="neato")
 simbad_er.attr('node', shape='box', penwidth='6', style='filled', color='lightgray')
 simbad_er.attr('graph', splines="curved", overlap="false", mode='major', esep='+4', sep='3')
-simbad_er.attr('edge', color='lightgray', penwidth='7')
+simbad_er.attr('edge', color='lightgray', penwidth='7', labelOverlay='100%')
 
 # Add the subgraph for measurement tables
 
@@ -46,14 +48,18 @@ mes_html_table = mes_html_table[:-2]
 mes_html_table += "}"
 
 simbad_er.node("Measurement tables", label=mes_html_table, shape="record")
-simbad_er.edge("basic", "Measurement tables", tooltip="oid:oidref")
+simbad_er.edge("basic", "Measurement tables", tooltip="oid:oidref", color=next(colors))
 
 
 for table in tables:
     simbad_er.node(str(table["table_name"]), tooltip=str(table["description"]))
 
 for link in links:
+    c = next(colors)
     simbad_er.edge(link["from_table"], link["target_table"],
-                   tooltip=f"{link["from_column"]}:{link["target_column"]}")
+                   tooltip=f"{link["from_column"]}:{link["target_column"]}",
+                   color=c,
+                   #headlabel=f"<<table border='0' cellborder='0'><tr><td bgcolor='white'>{link["from_column"]}:{link["target_column"]}</td></tr></table>>"
+                   )
 
 simbad_er.render("./simbad-er")
